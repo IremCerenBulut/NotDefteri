f50187baf2a179774246ee974cfe238e74adb20c
IremCerennBulut <iremcrnblt@gmail.com>
2023-02-01 17:11:00 +0300

    Add project files.

---
+++ NotDefteriApi/Data/Migrations/20230201113840_ilk.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotDefteriApi.Data.Migrations
+{
+    public partial class ilk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notlar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Baslik = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Icerik = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OlusturmaZamani = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DegistirmeZamani = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notlar", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Notlar",
+                columns: new[] { "Id", "Baslik", "DegistirmeZamani", "Icerik", "OlusturmaZamani" },
+                values: new object[] { 1, "Alınacaklar", new DateTime(2023, 2, 1, 14, 38, 40, 236, DateTimeKind.Local).AddTicks(6100), "1.Bot\n 2.Kot", new DateTime(2023, 2, 1, 14, 38, 40, 236, DateTimeKind.Local).AddTicks(6085) });
+
+            migrationBuilder.InsertData(
+                table: "Notlar",
+                columns: new[] { "Id", "Baslik", "DegistirmeZamani", "Icerik", "OlusturmaZamani" },
+                values: new object[] { 2, "Görevler", new DateTime(2023, 2, 1, 14, 38, 40, 236, DateTimeKind.Local).AddTicks(6102), "1.Ye\n 2.Dua et\n3.Sev", new DateTime(2023, 2, 1, 14, 38, 40, 236, DateTimeKind.Local).AddTicks(6102) });
+
+            migrationBuilder.InsertData(
+                table: "Notlar",
+                columns: new[] { "Id", "Baslik", "DegistirmeZamani", "Icerik", "OlusturmaZamani" },
+                values: new object[] { 3, "Gez & Gör", new DateTime(2023, 2, 1, 14, 38, 40, 236, DateTimeKind.Local).AddTicks(6103), "1.Muş\n 2.Van\n 3.Of", new DateTime(2023, 2, 1, 14, 38, 40, 236, DateTimeKind.Local).AddTicks(6103) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notlar");
+        }
+    }
+}
---
+++ NotDefteriApi/Data/UygulamaDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NotDefteriApi.Data
+{
+    public class UygulamaDbContext:DbContext
+    {
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Not> Notlar=> Set<Not>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Not>().HasData(
+                new Not() { Id=1,Baslik="Alınacaklar",Icerik="1.Bot\n 2.Kot"},
+                new Not() { Id=2,Baslik="Görevler",Icerik="1.Ye\n 2.Dua et\n3.Sev"},
+                new Not() { Id = 3, Baslik = "Gez & Gör", Icerik = "1.Muş\n 2.Van\n 3.Of" }
+                );
+        }
+    }
+}
